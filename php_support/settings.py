d81b76a47db6e07705eee181c29ab91a279f6a55
EgoisTa <97385729+EgoisTa-Git@users.noreply.github.com>
2023-02-21 23:57:49 +0300

    Add SECURE_PROXY_SSL_HEADER

--- php_support/settings.py
+++ php_support/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', [])
 
 CSRF_TRUSTED_ORIGINS = env.list('CSRF_TRUSTED_ORIGINS', [])
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Application definition
 
