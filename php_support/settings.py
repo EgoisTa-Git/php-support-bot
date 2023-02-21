d765ce863d73e75524839f431c27a797c48b18b5
EgoisTa <97385729+EgoisTa-Git@users.noreply.github.com>
2023-02-22 00:01:12 +0300

    Revert "Add SECURE_PROXY_SSL_HEADER"
    This reverts commit d81b76a47db6e07705eee181c29ab91a279f6a55.

--- php_support/settings.py
+++ php_support/settings.py
@@ -33,7 +33,6 @@ ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', [])
 
 CSRF_TRUSTED_ORIGINS = env.list('CSRF_TRUSTED_ORIGINS', [])
 
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Application definition
 
