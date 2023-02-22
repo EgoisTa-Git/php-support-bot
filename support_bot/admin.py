d95ec23aabf061fc6159c583b1669d0ce5c64f3e
EgoisTa <97385729+EgoisTa-Git@users.noreply.github.com>
2023-02-22 10:50:36 +0300

    Set titles in admin

--- support_bot/admin.py
+++ support_bot/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+admin.site.site_header = 'Сервис PHP Support'
+admin.site.site_title = 'PHP Support'
+admin.site.index_title = 'Панель администратора'
+
+
 # Register your models here.
