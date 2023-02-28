9085f313bf1bd15ce8ee22f800a423e30d255f4b
lexa.shvetsoff <lexa.shvetsoff@yandex.ru>
2023-02-28 18:10:12 +0800

    Регистрация заказчика, создание заказа

--- support_bot/management/commands/start_bot.py
+++ support_bot/management/commands/start_bot.py
@@ -1,6 +1,8 @@
 """Менеджмент команда запуска ТГ бота"""
 from support_bot.tg_frelance import handle_application_menu, handle_buy_subscribe,\
     handle_freelancer_register, handle_select_action, handle_select_order, handle_action_order
+from support_bot.tg_customer import handler_check_subscribe, conformation_tariff, save_customer, handle_main_customer, \
+    order_title, action_customer, order_start, order_description
 from support_bot.tg_bot_main import TGBot, handle_role, start
 from django.conf import settings
 from django.core.management import BaseCommand
@@ -13,8 +15,16 @@ class Command(BaseCommand):
                 {
                     "START": start,
                     "HANDLE_ROLE": handle_role,
-                    'HANDLE_APPLICATION_MENU': handle_application_menu,
-                    'HANDLE_BUY_SUBSCRIBE': handle_buy_subscribe,
+                    # 'HANDLE_APPLICATION_MENU': handle_application_menu,
+                    # 'HANDLE_BUY_SUBSCRIBE': handle_buy_subscribe,
+                    'HANDLE_APPLICATION_MENU': handle_main_customer,
+                    'HANDLE_BUY_SUBSCRIBE': conformation_tariff,
+                    'SAVE_CUSTOMER': save_customer,
+                    'ACTION_CUSTOMER': action_customer,
+                    'ORDER_START': order_start,
+                    'CUSTOMER_REG_ORDER_TITLE': order_title,
+                    'CUSTOMER_REG_ORDER_DESCRIPTION': order_description,
+                    'HANDLER_CHECK_SUBSCRIBE': handler_check_subscribe,
                     'HANDLE_FRELANCER_REGISTER': handle_freelancer_register,
                     'HANDLER_SELECT_ACTION': handle_select_action,
                     'HANDLE_SELECT_ORDER': handle_select_order,
--- support_bot/tg_customer.py
+++ support_bot/tg_customer.py
@@ -1,4 +1,6 @@
 """Модуль телеграм-бота для заказчика"""
+import datetime
+
 from telegram import ReplyKeyboardRemove, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ConversationHandler, MessageHandler, Filters
 
@@ -46,27 +48,49 @@ def get_user_orders():
     pass
 
 
+# HANDLER_CHECK_SUBSCRIBE
+def handler_check_subscribe(bot, update, context):
+    chat_id = context.user_data['chat_id']
+    user = context.user_data['user']
+    active_subscribe = Subscription.objects.filter(user=user, expire_at__gte=datetime.datetime.now()).exists()
+    print(active_subscribe)
+
+    if active_subscribe:
+        bot.send_message(chat_id=chat_id, text='Добро пожаловать!')
+        handle_main_customer(bot, update, context)
+        return 'ACTION_CUSTOMER'
+    else:
+        message_text = 'Для оплаты и активации подписки свяжитесь с менеджером {контакты менеджера}'
+        bot.send_message(chat_id=chat_id, text=message_text)
+
+
 # Стартовое сообщение заказчика
-def handle_main_customer(update, context):
-    message_text = 'Привет! С чего начнем?'
+def handle_main_customer(bot, update, context):
+    chat_id = context.user_data['chat_id']
+    message_text = 'С чего начнем?'
     reply_markup = customer_main_keyboard()
-    update.message.text(
-        text=message_text,
-        reply_markup=reply_markup
-    )
+    bot.send_message(chat_id=chat_id, text=message_text, reply_markup=reply_markup)
+    return 'ACTION_CUSTOMER'
 
 
-def customer_selected_tariff(update, context):
-    reply_markup = get_tariff_keyboard()
-    update.message.reply_text(
-        'Выбирете тариф',
-        reply_markup=reply_markup
-    )
-    return 'CONFIRMATION_TARIFF'
+def customer_selected_tariff(bot, update, context):
+    chat_id = context.user_data['chat_id']
+    if not update.callback_query:
+        return 'HANDLE_BUY_SUBSCRIBE'
+    action_selected = update.callback_query.data
+    user = context.user_data['user']
+    print(action_selected)
+    context.user_data['action'] = action_selected
+    if action_selected == 'client':
+        reply_markup = get_tariff_keyboard()
+        message_text = 'Выбирете тариф'
+        bot.send_message(chat_id=chat_id, text=message_text, reply_markup=reply_markup)
+        return 'HANDLE_BUY_SUBSCRIBE'
 
 
-def conformation_tariff(update, context):
-    tariff_id = update.message.text
+def conformation_tariff(bot, update, context):
+    chat_id = context.user_data['chat_id']
+    tariff_id = update.callback_query.data
     context.user_data['tariff_id'] = tariff_id
     tariff = Tariff.objects.get(id=int(tariff_id))
 
@@ -79,88 +103,104 @@ def conformation_tariff(update, context):
 Цена: {tariff.price}
 '''
     reply_markup = confirmation_tariff_keyboard()
-    update.message.reply_text(
-        text= description_tariff,
-        reply_markup=reply_markup
-    )
+    bot.send_message(chat_id=chat_id, text=description_tariff, reply_markup=reply_markup)
 
     return 'SAVE_CUSTOMER'
 
 
-def save_customer(update, context):
+def save_customer(bot, update, context):
+    chat_id = context.user_data['chat_id']
     tariff_id = context.user_data['tariff_id']
     selected_tariff = Tariff.objects.get(id=int(tariff_id))
 
-    chat_id = update.message.chat_id
-    client = CustomUser.get(tg_id=chat_id)
-    Subscription.objects.create(
+    # chat_id = update.message.chat_id
+    client = CustomUser.objects.get(tg_id=chat_id)
+    subscription = Subscription.objects.create(
         user=client,
         tariff=selected_tariff,
         expire_at=timezone.now()
     )
-    bot_state = handle_main_customer(update, context)
+    subscription.save()
+
+    # bot_state = handle_main_customer(bot, update, context)
+    # message_text = 'Для оплаты и активации подписки свяжитесь с менеджером {контакты менеджера}'
     message_text = 'Для оплаты и активации подписки свяжитесь с менеджером {контакты менеджера}'
-    update.message.reply_text(text=message_text)
-    return bot_state
+    bot.send_message(chat_id=chat_id, text=message_text)
+    return 'HANDLER_CHECK_SUBSCRIBE'
 
 
-def back_tariffs(update, context):
+def back_tariffs(bot, update, context):
+    customer_selected_tariff(bot, update, context)
     return 'CONFIRMATION_TARIFF'
 
 
-# Регистрация заказчика
-registr_customer = ConversationHandler(
-    entry_points=[],
-    states={},
-    fallbacks=[]
-)
-
-
 # Создание заказа клиентом
-def order_start(update, context):
-    update.message.reply_text(
-        "Введите заголовок заказа",
-        reply_markup=ReplyKeyboardRemove()
-    )
+def action_customer(bot, update, context):
+    chat_id = context.user_data['chat_id']
+    if update.callback_query.data == 'Разместить заказ':
+        message_text = 'Начнем создание заказа'
+        bot.send_message(chat_id=chat_id, text=message_text)
+        # state = 'ORDER_START'
+        return order_start(bot, update, context)
+    elif update.callback_query.data == 'Мои заказы':
+        return 'SHOW_ORDERS'
+
+
+def order_start(bot, update, context):
+    chat_id = context.user_data['chat_id']
+    # update.message.reply_text(
+    #     "Введите заголовок заказа",
+    #     reply_markup=ReplyKeyboardRemove()
+    # )
+    message_text = 'Введите заголовок заказа'
+    bot.send_message(chat_id=chat_id, text=message_text)
     return 'CUSTOMER_REG_ORDER_TITLE'
 
 
-def order_title(update, context):
+def order_title(bot, update, context):
+    chat_id = context.user_data['chat_id']
     title_order = update.message.text
     if len(title_order) == 0:
-        update.message.reply_text('Введите заголовок заказа')
+        message_text = 'Введите заголовок заказа'
+        bot.send_message(chat_id=chat_id, text=message_text)
         return 'CUSTOMER_REG_ORDER_TITLE'
     else:
         context.user_data['order'] = {'title': title_order}
-        update.message.reply_text('Введите описание заказа')
+        message_text = 'Введите описание заказа'
+        bot.send_message(chat_id=chat_id, text=message_text)
         return 'CUSTOMER_REG_ORDER_DESCRIPTION'
 
 
-def order_description(update, context):
+def order_description(bot, update, context):
+    chat_id = context.user_data['chat_id']
     description_order = update.message.text
     if len(description_order) == 0:
-        update.message.reply_text('Введите описание заказа')
+        message_text = 'Введите описание заказа'
+        bot.send_message(chat_id=chat_id, text=message_text)
         return 'CUSTOMER_REG_ORDER_DESCRIPTION'
     else:
         context.user_data['order']['description'] = description_order
 
         # Сохранение заказа в бд
-        chat_id = update.message.chat_id
-        author = CustomUser.get(tg_id=chat_id)
+        chat_id = context.user_data['chat_id']
+        author = CustomUser.objects.get(tg_id=chat_id)
         try:
-            Request.objects.create(
+            customer_request =Request.objects.create(
                 title=context.user_data['order']['title'],
                 description=description_order,
                 author=author,
             )
+            customer_request.save()
             subscription = Subscription.objects.get(user=author)
             subscription.requests_created = F('requests_created') + 1
             subscription.save()
         except:
             print('Шеф, всё пропало!')
-        bot_state = handle_main_customer(update, context)
-        update.message.reply_text('Заказ размещен!')
-        return bot_state
+        bot.send_message(chat_id=chat_id, text='Заказ размещен!')
+        # bot_state = handle_main_customer(bot, update, context)
+
+        # update.message.reply_text('Заказ размещен!')
+        return handle_main_customer(bot, update, context)
 
 
 def order_dontknow(update, context):
